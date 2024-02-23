bfd3b0eb93a0caf67920c827323b28c85eedf182
Josec690 <jose.silva690@fatec.sp.gov.br>
2024-02-23 08:38:29 -0300

    primeira aula de node

---
+++ app.js
@@ -0,0 +1,29 @@
+/*console.log("Mensagem de Texto")
+
+var n1 = 10
+var n2 = 2
+
+var adic = n1 + n2
+var sub = n1 - n2
+var mult = n1 * n2
+var div = n1 / n2
+
+console.log("Adição: " + adic)
+console.log("Subtração: " + sub)
+console.log("Multiplicação: " + mult)
+console.log("divisão: " + div)*/
+
+/* if/else
+while /for /do while*/
+
+var adicao = require('./adicao')
+console.log(adicao(10,2))
+
+var subtracao = require('./subtracao')
+console.log(subtracao(10,2))
+
+var multiplicacao = require('./multiplicacao')
+console.log(multiplicacao(10,2))
+
+var divisao = require('./divisao')
+console.log(divisao(10,2))
